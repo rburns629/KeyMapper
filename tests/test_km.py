02c68851584d0d0056e960922859088b1292392a
Robert Burns <robert.burns1@ikea.com>
2018-05-11 23:45:07 -0400

    Removed a test

--- tests/test_km.py
+++ tests/test_km.py
@@ -16,30 +16,3 @@ if __name__ == '__main__':
     # What happens when you have an iterable...?
     print(km_dict['d.a'][0]['d'])
     print(km_dict['d.a.[0].d'])  # Can use: 0-9, [0-9], (0-9), {0-9}
-
-    if re.match(r'((y|Y)es)', input('Continue tests? (yes/no): ')) is not None:
-        questions = ['What is your name: ', 'What is your email: ', 'What is your password: ', 'How many servers do you want to add: ', 'What is the ip: ', 'What is the port: ']
-        keys = ['user.name', 'user.email', 'user.password', 'servers']
-        config = {'user': {'name': '', 'email': '', 'password': ''}, 'servers': []}
-
-        km_dict = KeyMapper(config)
-
-        for i in range(len(questions)):
-            if i < 3:
-                km_dict[keys[i]] = input(questions[i])
-            else:
-                for r in range(int(input(questions[i]))):
-                    server_info = {'ip': '', 'port': ''}
-                    for i, k in enumerate(questions[4:]):
-                        if i < 1:
-                            server_info['ip'] = input(k)
-                        else:
-                            server_info['port'] = input(k)
-                    km_dict[keys[len(keys) - 1]].append(server_info)
-                break
-        print(km_dict)  # Prints: ... Well, whatever you entered as your input values! Try it if you don't believe me ;)
-        for i in range(len(km_dict['servers'])):
-            print(km_dict['servers.{}.ip'.format(i)])  # Prints the IP for each index iterated through
-    else:
-        print('Exiting...')
-        exit()
